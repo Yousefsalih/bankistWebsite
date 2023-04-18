e20ce24a4cd5722633e1095cc37be796928bb456
yousef <yousef_salih@hotmail.com>
2023-04-18 12:31:06 +0300

    Page navigation using event delegation

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
+// Selecting Elements
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');//Learn more Button in the header
+const section1 = document.querySelector('#section--1');//Scrolling to this section
 
+
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -31,10 +36,24 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');//Learn more Button in the header
-const section1 = document.querySelector('#section--1');//Scrolling to this section
-
+///////////////////////////////////////
 //Implementing smooth scrolling
 btnScrollTo.addEventListener('click', function(){
   section1.scrollIntoView({ behavior: 'smooth' });
+})
+
+///////////////////////////////////////
+//Page navigation
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  console.log(e.target); //which is .nav__links is the ul (parent element) and e.target will include it when clicking
+
+  //Matching strategy - ignores clicks if it didnt happen on the link (i.e on the ul)
+  if (e.target.classList.contains('nav__link')) {
+    // console.log('LINK'); //Shows only links
+        e.preventDefault();//removes the default behaviour of scrolling automatically to the section
+        // console.log('LINK');
+        const id = e.target.getAttribute('href')
+        // console.log(id);
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 })
